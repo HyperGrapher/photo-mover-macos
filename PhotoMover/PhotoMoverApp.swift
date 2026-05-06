a3cee333f837112a2c28b393e0fa2d9a9297da8f
Burak Mestan <burakmestan@gmail.com>
2026-05-06 20:28:42 +0300

    Initial Commit

---
+++ PhotoMover/PhotoMoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMoverApp.swift
+//  PhotoMover
+//
+//  Created by Burak on 6.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
